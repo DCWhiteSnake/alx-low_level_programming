dff7e226d85e07ef5983d8fd9d7b51df6d894480
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-16 18:58:58 -0800

    complete

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void);
+/**
+ * main - Print the same message to the console
+ * Return: 0 if no error
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Print the same message to the console
+ * Return: 0 always without error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
