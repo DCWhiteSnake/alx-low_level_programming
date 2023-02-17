aacb80f18ad4fc09cc18fa1987dac89b1fde4150
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-17 14:58:34 -0800

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - prints out the last digit from a randomly generated integer
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+	int last_d;
+	char *str_n = malloc(sizeof(char) * 10);
+	char *ex_c;
+	int nc = 0;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	sprintf(str_n, "%d", (int)fabs((double)n));
+	nc = strlen(str_n);
+	last_d = atoi(&str_n[strlen(str_n) - 1]);
+	if (last_d == 0)
+	{
+		ex_c = "and is 0";
+	}
+	else if (last_d > 5)
+	{
+		ex_c = "and is greater than 5";
+	}
+	else
+	{
+		ex_c = "and is less than 6 and not 0";
+	}
+	printf("The last digit of %i is %i %s\n", n, last_d, ex_c);
+	free(str_n);
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -3,4 +3,6 @@
   The main function here uses the time, srand and rand functions to generate
   random numbers >= 0 with the goal of checking if the number is negative, 0, or
   positve, it does this by printing the result to the console.
-
+- 1-last_digit
+  Using atoi and sprintf the main function parses the value of the last digit of
+  the randomly generated number and performs some logic on it.
