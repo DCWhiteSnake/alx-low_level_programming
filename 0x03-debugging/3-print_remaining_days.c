5b7fce393f7e4a97c0d57458a55fef6e6006eade
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-23 10:37:48 -0800

    fix-5 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -25,27 +25,16 @@ void print_remaining_days(int month, int day, int year)
 	else if ((year % 100 == 0 && year % 400 == 0) && year % 4 != 0)
 	{
 		if (month > 2)
-                {
-                        day++;
-                }
-
-                printf("Day of the year: %d\n", day);
-                printf("Remaining days: %d\n", 366 - day);
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-	        int max_days[] = {31, 59, 90,120, 151, 181, 212, 243, 273,
-					304, 334, 365};
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
 
-		if (day < 1 || day > (max_days[month - 1]))
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month,
-			       day - max_days[month-2], year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
 	}
 }
