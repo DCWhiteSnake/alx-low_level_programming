364ac3137b64bd494b9862dfb9dbbb1a94de32c2
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-23 14:20:53 -0800

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: 0 since there little room for errors
+ */
+void print_numbers(void);
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar(10);
+}
