48acd6ac95c396c4b2b858f1c19b43c0de9f87b0
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-24 07:14:49 -0800

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,12 +8,16 @@
 void print_diagonal(int n)
 {
 	int i;
+	int k;
 
 	if (!(n <= 0))
 	{
 		for (i = 0; i < n; i++)
 		{
+			for (k = i; k >= 0; k--)
+				_putchar(32);
 			_putchar(92);
+			_putchar(10);
 		}
 	}
 	_putchar(10);
