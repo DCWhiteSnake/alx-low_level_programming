6a834967322693276b3a73fa9225f7bc0ee8a919
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-23 17:24:21 -0800

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - print (_ * n) and then append $
+ * @n: number of times to print '_'
+ * Return: 0 on completion
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar(36);
+	_putchar(10);
+}
