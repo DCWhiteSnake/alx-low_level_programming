82acaed7fc13bac3864d10040cef5b648b507a72
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-23 17:30:19 -0800

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,13 @@
 void print_line(int n)
 {
 	int i;
-
-	for (i = 1; i <= n; i++)
+	if (!(n <= 0))
 	{
-		_putchar(95);
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar(36);
 	}
-	_putchar(36);
 	_putchar(10);
 }
