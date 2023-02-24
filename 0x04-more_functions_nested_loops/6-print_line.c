d5101c89af164265fe74f92f702b7c6d4bd63c1c
DCWhiteSnake <davidokeke.c@gmail.com>
2023-02-23 17:53:12 -0800

    6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_line - prints straight line n times.
@@ -9,9 +9,12 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (!(n <= 0))
 	{
-		_putchar(95);
+		for (i = 0; i < n; i++)
+		{
+			_putchar(95);
+		}
 	}
 	_putchar(10);
 }
